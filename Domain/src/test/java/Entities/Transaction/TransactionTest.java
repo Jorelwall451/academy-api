2adb99953b9a1996ef937e9631c8799225a36da6
jorelwall451 <jorelwall154@gmail.com>
2023-12-27 19:52:36 -0300

    Add TransactionTest

---
+++ Domain/src/test/java/Entities/Transaction/TransactionTest.java
@@ -0,0 +1,34 @@
+package Entities.Transaction;
+
+import Entities.User.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public final class TransactionTest {
+    private final UUID transactionId = UUID.randomUUID();
+    private final User fromUser = new User(UUID.randomUUID());
+    private  final User toUser = new User(UUID.randomUUID());
+    private final Transaction transaction = new Transaction(transactionId, fromUser.getId(), toUser.getId());
+
+    @Test
+    public void testGetId(){
+        Assertions.assertEquals(transactionId, transaction.getId());
+    }
+
+    @Test
+    public void testGetSetAmount() {
+        Float amount = 100.0f;
+        transaction.setAmount(amount);
+        Assertions.assertEquals(amount, transaction.getAmount());
+    }
+
+    @Test
+    public void testGetSetCreatedAt() {
+        LocalDateTime createdAt = LocalDateTime.now();
+        transaction.setCreatedAt(createdAt);
+        Assertions.assertEquals(createdAt, transaction.getCreatedAt());
+    }
+}
