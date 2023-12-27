db1f2694ae8e792184560cbe42042ea78834d6b4
jorelwall451 <jorelwall154@gmail.com>
2023-12-27 18:52:02 -0300

    Add Plan Entity

---
+++ Domain/src/main/java/Entities/Plan/Plan.java
@@ -0,0 +1,61 @@
+package Entities.Plan;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public final class Plan {
+    private final UUID id;
+    private String name;
+    private String description;
+    private Float price;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Plan(UUID id){
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
