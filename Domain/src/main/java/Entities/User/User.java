d3a5f16e1a26d5caf2903ca0aff7fcdf64f3a7ab
jorelwall451 <jorelwall154@gmail.com>
2023-12-27 00:12:33 -0300

    Add User entity

---
+++ Domain/src/main/java/Entities/User/User.java
@@ -0,0 +1,128 @@
+package Entities.User;
+
+import ValueObjects.Cpf;
+import ValueObjects.Email;
+import ValueObjects.PixKey;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+    private final UUID id;
+    private String name;
+    private String description;
+    private String password;
+    private String photoUrl;
+    private Integer age;
+    private Email email;
+    private Cpf cpf;
+    private PixKey pixKey;
+    private LocalDateTime birthdate;
+    private LocalDateTime loginAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public Cpf getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Cpf cpf) {
+        this.cpf = cpf;
+    }
+
+    public PixKey getPixKey() {
+        return pixKey;
+    }
+
+    public void setPixKey(PixKey pixKey) {
+        this.pixKey = pixKey;
+    }
+
+    public LocalDateTime getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDateTime birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public LocalDateTime getLoginAt() {
+        return loginAt;
+    }
+
+    public void setLoginAt(LocalDateTime loginAt) {
+        this.loginAt = loginAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
