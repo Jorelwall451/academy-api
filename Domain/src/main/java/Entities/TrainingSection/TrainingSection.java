d201ae3459ece9d88e2dcde3a96bfd465dc88056
jorelwall451 <jorelwall154@gmail.com>
2023-12-27 18:52:25 -0300

    Add TrainingSection Entity

---
+++ Domain/src/main/java/Entities/TrainingSection/TrainingSection.java
@@ -0,0 +1,64 @@
+package Entities.TrainingSection;
+
+import Entities.Training.Training;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public final class TrainingSection {
+    private final UUID id;
+    private List<Training> trainings;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public TrainingSection(UUID id){
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public List<Training> getTrainings() {
+        return trainings;
+    }
+
+    public void setTrainings(List<Training> trainings) {
+        this.trainings = trainings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
