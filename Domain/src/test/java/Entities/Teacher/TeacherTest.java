e54a86c1d2926de408e3994ca41ed954b5f6b45d
jorelwall451 <jorelwall154@gmail.com>
2023-12-27 23:39:08 -0300

    Add TeacherTest

---
+++ Domain/src/test/java/Entities/Teacher/TeacherTest.java
@@ -0,0 +1,54 @@
+package Entities.Teacher;
+
+import Entities.Assessment.Assessment;
+import Entities.Student.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public final class TeacherTest {
+    private final UUID teacherId = UUID.randomUUID();
+    private final Teacher teacher = new Teacher(teacherId);
+
+    @Test
+    public void testGetId(){
+        Assertions.assertEquals(teacherId, teacher.getId());
+    }
+
+    @Test
+    public void testGetSetStudents(){
+        List<Student> testStudents = new ArrayList<>();
+
+        for(int studentIndex = 0; studentIndex < 10; studentIndex++){
+            Student newTestStudent = new Student(UUID.randomUUID());
+
+            testStudents.add(newTestStudent);
+        }
+
+        teacher.setStudents(testStudents);
+
+        for(int studentIndex = 0; studentIndex < 10; studentIndex++){
+            Assertions.assertEquals(testStudents.get(studentIndex), teacher.getStudents().get(studentIndex));
+        }
+    }
+
+    @Test
+    public void testGetSetAssessments(){
+        List<Assessment> testAssessments = new ArrayList<>();
+
+        for(int assessmentIndex = 0; assessmentIndex < 10; assessmentIndex++){
+            Assessment newAssessment = new Assessment(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
+
+            testAssessments.add(newAssessment);
+        }
+
+        teacher.setAssessments(testAssessments);
+
+        for(int assessmentIndex = 0; assessmentIndex < 10; assessmentIndex++){
+            Assertions.assertEquals(testAssessments.get(assessmentIndex), teacher.getAssessments().get(assessmentIndex));
+        }
+    }
+}
