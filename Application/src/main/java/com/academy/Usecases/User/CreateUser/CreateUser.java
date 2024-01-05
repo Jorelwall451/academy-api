db4e7ff1fef0edb6bb12d6528517b38fefac6a3d
jorelwall451 <jorelwall154@gmail.com>
2024-01-04 23:17:29 -0300

    Add application CreateUser

---
+++ Application/src/main/java/com/academy/Usecases/User/CreateUser/CreateUser.java
@@ -0,0 +1,11 @@
+package com.academy.Usecases.User.CreateUser;
+
+import com.academy.Shared.Usecase;
+
+public final class CreateUser implements Usecase<InputBoundary, OutputBoundary> {
+
+    @Override
+    public OutputBoundary handle(InputBoundary o) {
+        return null;
+    }
+}
